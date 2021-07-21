209feb4e353095c0da9ebd34ce519bded9c8ba0c
andreneveu <40238860+andreneveu@users.noreply.github.com>
2021-07-21 13:36:44 -0400

    book updates

--- recessions_and_dates_all.R
+++ recessions_and_dates_all.R
@@ -11,7 +11,7 @@ recessions.df = read.table(textConnection(
   1990-07-01, 1991-03-01
   2001-03-01, 2001-11-01
   2007-12-01, 2009-06-01
-  2020-02-01, 2021-06-01"), sep=',',
+  2020-02-01, 2020-04-01"), sep=',',
   colClasses=c('Date', 'Date'), header=TRUE)
 
 startdate <- round_date(Sys.Date() - lubridate::years(25),"month")
